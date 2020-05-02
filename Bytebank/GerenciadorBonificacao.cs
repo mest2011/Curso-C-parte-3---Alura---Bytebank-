3a67b034173a08c0546dccec39ea76f09c200cf9
mest2011 <mest2011mest@gmail.com>
2020-05-02 20:25:21 -0300

    Primerira aula

---
+++ Bytebank/GerenciadorBonificacao.cs
@@ -0,0 +1,21 @@
+﻿using Bytebank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bytebank
+{
+    class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
---
+++ Bytebank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Bytebank.Funcionarios;
+
+namespace Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "395.470.068-94";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
